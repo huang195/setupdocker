01d4e5f7f9320ae17072344b2702ae02825a10e0
HAI HUANG <huang195@gmail.com>
2015-10-20 12:04:43 -0400

    initial checkin

---
+++ README.md
@@ -0,0 +1,6 @@
+Setup docker on a local host running Ubuntu 14.04 Trusty
+- setup gpg key
+- setup apt-get
+- install docker engine
+- change docker backend storage to btrfs
+- start docker engine service
---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+apt-key adv --keyserver hkp://pgp.mit.edu:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" > /etc/apt/sources.list.d/docker.list
+apt-get update
+apt-get purge lxc-docker*
+apt-get install -y docker-engine
+
+service docker stop
+rm -rf /var/lib/docker/*
+cd /var/lib
+dd if=/dev/zero of=disk bs=1 count=0 seek=50G
+apt-get install -y btrfs-tools
+mkfs.btrfs disk
+mount -o loop disk docker
+service docker start
